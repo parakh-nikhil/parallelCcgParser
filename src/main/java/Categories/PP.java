5966c201e5ebb2a16f365620eeedd07d4def00ee
Nikhil Parakh <nikhilparakh@Nikhils-MacBook-Pro-2.local>
2023-02-09 01:13:27 -0500

    Determiner and Prepositional Phrase added

---
+++ src/main/java/Categories/PP.java
@@ -0,0 +1,10 @@
+package Categories;
+
+public class PP extends AtomicCategory{
+    String preposition;
+
+    public PP(String preposition){
+        super();
+        this.preposition = preposition;
+    }
+}
