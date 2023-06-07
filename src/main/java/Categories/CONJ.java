b7e3b2d5412ce3674549531e877306e5a62a2b5b
Nikhil Parakh <nikhilparakh@Nikhils-MacBook-Pro-2.local>
2023-06-06 21:33:15 -0400

    CONJ and PUNC made singleton

--- src/main/java/Categories/CONJ.java
+++ src/main/java/Categories/CONJ.java
@@ -1,6 +1,15 @@
 package Categories;
 
 public class CONJ extends AtomicCategory{
+    private static CONJ instance;
+    private CONJ(){}
+
+    public static CONJ getInstance(){
+        if (instance==null){
+            instance = new CONJ();
+        }
+        return instance;
+    }
     @Override
     public String toString() {
         return "CONJ";
--- src/main/java/Categories/PUNC.java
+++ src/main/java/Categories/PUNC.java
@@ -1,6 +1,16 @@
 package Categories;
 
 public class PUNC extends AtomicCategory{
+    private static PUNC instance;
+
+    private PUNC(){}
+
+    public static PUNC getInstance(){
+        if (instance==null){
+            instance = new PUNC();
+        }
+        return instance;
+    }
     @Override
     public String toString() {
         return "PUNC";
